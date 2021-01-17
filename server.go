39e8440ff0e7e70e503c8e7c07e0478eef6f3e7d
Rafael Marini <rfaelmarini@gmail.com>
2021-01-16 21:28:56 -0300

    Validate queryparam and integrate cat api

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,9 +12,29 @@ import (
 func main() {
 	server := gin.Default()
 	server.GET("/breeds", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
-		})
+		name, ok := ctx.GetQuery("name")
+		if !ok {
+			ctx.JSON(http.StatusOK, []string{})
+			ctx.Abort()
+			return
+		}
+
+		url := "https://api.thecatapi.com/v1/breeds/search?api_key=1a9c1e22-9dc7-48fa-844c-5d137e80694&q=" + name
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if resp.Body != nil {
+			defer resp.Body.Close()
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		ctx.JSON(http.StatusOK, json.RawMessage(string(body)))
 	})
 
 	server.Run(":8080")
