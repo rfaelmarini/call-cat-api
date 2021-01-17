72a17e6e5bd4f6418cee4182bb913d99b94b4586
Rafael Marini <rfaelmarini@gmail.com>
2021-01-16 21:32:59 -0300

    set env variables

--- server.go
+++ server.go
@@ -5,11 +5,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func setEnvVariables() {
+	os.Setenv("API_KEY", "1a9c1e22-9dc7-48fa-844c-5d137e80694")
+}
+
 func main() {
+	setEnvVariables()
 	server := gin.Default()
 	server.GET("/breeds", func(ctx *gin.Context) {
 		name, ok := ctx.GetQuery("name")
@@ -19,7 +25,8 @@ func main() {
 			return
 		}
 
-		url := "https://api.thecatapi.com/v1/breeds/search?api_key=1a9c1e22-9dc7-48fa-844c-5d137e80694&q=" + name
+		apiKey := os.Getenv("API_KEY")
+		url := "https://api.thecatapi.com/v1/breeds/search?api_key=" + apiKey + "&q=" + name
 		resp, err := http.Get(url)
 		if err != nil {
 			log.Fatalln(err)
